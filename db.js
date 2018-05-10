d184196454a1f030fe6b409234446ebada86de60
Macbook Pro <macbookpro@MacBook-Pro-Oleg.local>
2018-05-11 00:57:39 +0300

    sdf

--- db.js
+++ db.js
@@ -3,35 +3,52 @@ var MongoClient = require('mongodb').MongoClient;
 // Connection URL 
 var url = "mongodb://Admin2:InstaAnon811@ds161029.mlab.com:61029/heroku_chq8wgqb";
 
-async function connect(callback) {
-  // Use connect method to connect to the server
-  let DBInst = await MongoClient.connect(url);
-  return DBInst;
-}
+// async function connect(callback) {
+//   // Use connect method to connect to the server
+//   let DBInst = await MongoClient.connect(url);
+//   return DBInst;
+// }
 
-async function recordUserSearch(userName) {
-  // Get the documents collection
-  var collection = DBInst.collection('users');
-  // update
-  var date = new Date().getDate();
-  var user = await collection.findOne({ userName: userName, date: date });
-  if (user) {
-    user.count++;
-    let pro = collection.update({
-      userName: user.userName,
-      date: user.userName,
-      }, oUser, {
-        upsert: true
-      });
-  } else {
-    user.userName = userName,
-    user.date = date,
-    user.count = 1;
-    collection.insert(user);
+
+// db class
+class DBClass {
+  constructor() {
+    this.DBInst = {};
+    //this.connect();
   }
 
+  async connect() {
+    var client = await MongoClient.connect(url);
+    this.DBInst = client.db("heroku_chq8wgqb");
+  }
+
+  async recordUserSearch(userName) {
+    // Get the documents collection
+    var collection = this.DBInst.collection("users");
+    // update
+    var currDate = new Date(),
+        date = currDate.getDate() + '.' + (currDate.getMonth() + 1) + '.' + currDate.getFullYear();
+    var user = await collection.findOne({ userName: userName, date: date });
+    if (user) {
+      user.count++;
+      let pro = collection.update({
+        userName: user.userName,
+        date: user.date,
+      }, user, {
+        upsert: true
+      });
+    }
+    else {
+      user = {
+        userName : userName,
+        date : date,
+        count : 1
+      };
+      collection.insert(user);
+    }
+
+  }
 
 }
 
-exports.connect = connect;
-exports.recordUserSearch = recordUserSearch;
+exports.DBClass = DBClass;
