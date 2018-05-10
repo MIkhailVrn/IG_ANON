fe3f5e975d88973a8ca5d7ed6b6a16eb747fd5cb
Macbook Pro <macbookpro@MacBook-Pro-Oleg.local>
2018-05-11 01:14:51 +0300

    sdf

--- server.js
+++ server.js
@@ -1,23 +1,23 @@
 var http = require("http"),
-    url = require("url"),
-    DB = require("./db.js");
+url = require("url"),
+DB = require("./db.js");
 
 
 async function start(route, handle) {
 
-  var DBInst = new DB.DBClass();
-  await DBInst.connect();
+var DBInst = new DB.DBClass();
+await DBInst.connect();
 
-  function onRequest(request, response) {
-    var pathname = url.parse(request.url).pathname,
-        params =  url.parse(request.url, true).query;
-    
-    console.log("Request for " + pathname + " received.");
-    route(handle, pathname, params, response, request, DBInst);
-  }
+function onRequest(request, response) {
+var pathname = url.parse(request.url).pathname,
+    params =  url.parse(request.url, true).query;
 
-  http.createServer(onRequest).listen(process.env.PORT);
-  console.log("Server has started.");
+console.log("Request for " + pathname + " received.");
+route(handle, pathname, params, response, request, DBInst);
 }
- 
+
+http.createServer(onRequest).listen(process.env.PORT);
+console.log("Server has started.");
+}
+
 exports.start = start;
